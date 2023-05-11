2e957f9c161516ed6b7974ab5b31907e4874f238
Gabriel Zarzana Espinoza <gabriel.zarzana@gmail.com>
2023-05-11 03:44:01 -0300

    Added image.

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,15 @@
+import forca0 from "../assets/forca0.png";
+// import forca1 from "../assets/forca1.png";
+// import forca2 from "../assets/forca2.png";
+// import forca3 from "../assets/forca3.png";
+// import forca4 from "../assets/forca4.png";
+// import forca5 from "../assets/forca5.png";
+// import forca6 from "../assets/forca6.png";
+
 function Game() {
     return (
         <div className="Game">
-
+            <img src={forca0} data-test="game-image" />
         </div>
     );
 }
